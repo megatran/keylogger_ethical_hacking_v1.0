c20701bab9266c60de7817d095359c82801fef7c
Nhan Tran <nhantrantnt@gmail.com>
2016-12-25 21:02:08 -0700

    added helper header to encrypt message

---
+++ Base64.h
@@ -0,0 +1,32 @@
+#ifndef BASE64_H
+#define BASE64_H
+
+#include <vector>
+#include <string>
+ //encode and decode string
+
+ namespace Base64
+ {
+     std::string base64_encode(const std::string &);
+
+     const std::string &SALT1 = "LM:TB:BB";
+     const std::string &SALT2 = "_:/_77"
+     const std::string &SALT3 = "line=wowC++";
+
+     std:string EncryptB64(std::string s)
+     {
+         s = SALT1 + s + SALT2 + SALT3;
+         s = base64_encode(s);
+         s.insert(7, SALT3);
+         s += SALT1;
+         s = base64_encode(s);
+         s = SALT2 + SALT3 + SALT1;
+         s = base64_encode(s);
+         s.insert(1, "L");
+         s.insert(7, "m");
+         return s;
+     }
+
+ }
+
+#endif // BASE64_H
